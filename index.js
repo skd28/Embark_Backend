4295dbc047153b3d006450cd0998f866ebeafe80
skd28 <shivajaldwivedi@gamil.com>
2024-06-20 17:46:29 +0530

    change some cors

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //app.use(cors());
-app.use(cors({
-    origin: 'http://localhost:5173' // or use '*' to allow all origins
-}));
+app.use(cors());
 
 app.use(express.json());
 
