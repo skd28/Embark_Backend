f3757ddd3b78356e9f4e491627a9f5868c4348ca
skd28 <shivajaldwivedi@gamil.com>
2024-06-20 17:34:43 +0530

    update some blogs pages

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+//app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173' // or use '*' to allow all origins
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
